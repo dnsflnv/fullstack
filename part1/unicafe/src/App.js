e99ce0795e5ef00616e831edf68e1c4ab4b923b9
Denis Filonov <denis@filonov.pro>
2021-12-23 16:13:43 +0100

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,16 +4,30 @@ const Header = ({ headerText }) => <h1>{headerText}</h1>;
 
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
-const Statistics = ({ good, neutral, bad }) =>
-  <div>
-    <h2>Statistics</h2>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-  </div>;
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  let averange = 0;
+  let positive = 0;
+
+  if (all > 0) {
+    averange = good / all - bad / all;
+    positive = good * 100 / all;
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Averange: {averange}</p>
+      <p>Positive: {positive}%</p>
+    </div>
+  );
+};
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
