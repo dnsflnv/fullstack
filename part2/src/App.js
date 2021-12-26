feaf75ff70d98b397e00acc0fc2199c9b6567f36
Denis Filonov <denis@filonov.pro>
2021-12-26 19:24:15 +0100

    2.5

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,52 +1,5 @@
 import React from 'react';
-
-const Header = (props) => {
-  return (
-    <h1>{props.text}</h1>
-  );
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name}: {part.exercises}
-    </p>
-  );
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  );
-}
-
-const Total = ({ parts }) => {
-  // let totalEx = 0;
-  // parts.forEach(element => {
-  //   totalEx = totalEx + element.exercises;
-  // });
-  let initialVal = 0;
-  let totalEx = parts.reduce((s, p) => {
-    return s + p.exercises;
-  }, initialVal);
-  return (
-    <p><strong>Total of {totalEx} exercises</strong></p>
-  );
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header text={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
@@ -94,14 +47,12 @@ const App = () => {
     }
   ]
 
-
   return (
     <div>
       {courses.map(course =>
         <Course key={course.id} course={course} />
       )}
     </div>
-
   );
 }
 
